c784e3f2f723c99707fb2a8f7158dc69598fe4a7
Maria Vitória Amorim Farias Barbosa <101279006+AIROTIVBR@users.noreply.github.com>
2023-04-04 00:06:15 -0300

    Update project2.js

--- project2.js
+++ project2.js
@@ -2,9 +2,9 @@ function Converter() {
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
     var valorEmReal = parseFloat(valor);
-    var valorEmYuan = valorEmReal * 1.39;
+    var valorEmIene = valorEmReal * 26.22;
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O valor da conversão é ¥" + valorEmYuan.toFixed(2);
+    var valorConvertido = "O valor da conversão é ¥" + valorEmIene.toFixed(2);
     elementoValorConvertido.innerHTML = valorConvertido;
   }
   
